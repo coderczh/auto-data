c5243cab165846767793dca5b97295adb8fdf7d6
天天向上 <imchenzh@foxmail.com>
2025-12-29 17:13:37 +0800

    20251229_01提交

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,4 +6,20 @@ import { RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style scoped></style>
+<style lang="scss">
+// 重置全局边距
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+// 确保 body 和 html 也没有额外间距
+html,
+body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="">
-    <el-button type="primary">Primary</el-button>
+  <div class="login-container">
+    <div class="login">111</div>
   </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background: url(@/static/image/login-bgd.svg);
+
+  .login {
+    width: 500px;
+    height: 500px;
+    border: 1px solid #ccc;
+  }
+}
+</style>
